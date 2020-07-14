9309618324b8efd452a58b5c365e92de1b416a8b
unknown <jonathanm@itworks.com>
2020-07-13 19:32:02 -0600

    Add pokemon look up and display its information

--- src/components/PokemonSearch.tsx
+++ src/components/PokemonSearch.tsx
@@ -1,18 +1,108 @@
 import React, { Component } from 'react'
 import User from '../interfaces/User.interfaces'
 
-export class PokemonSearch extends Component<User> {
+interface searchState{
+    error: boolean,
+    pokemon: Pokemon,
+    jsonResponse: string
+}
+
+interface Pokemon{
+    name: string,
+    numberOfAbilities: number,
+    baseExperience: number,
+    imageUrl: string
+}
+
+export class PokemonSearch extends Component<User, searchState> {
+    
+    pokemonRef: React.RefObject<HTMLInputElement>;
+
+    constructor (props: User){
+        super(props);
+        this.state = {
+            error: false,
+            pokemon: {
+                name: "",
+                numberOfAbilities: 0,
+                baseExperience: 0,
+                imageUrl: ""
+            },
+            jsonResponse: ""
+        }
+        this.pokemonRef = React.createRef();
+    } 
+
+    onSearchClick = () => {
+        const inputValue = this.pokemonRef.current?.value;
+
+        if(inputValue){
+            fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`).then(res => {
+                if(res.status !== 200){
+                    this.setState({ error: true});
+                    return
+                }else{
+                    res.json().then(data =>{
+                        this.setState({
+                            error:false,
+                            pokemon: {
+                                name: data.name,
+                                numberOfAbilities: data.abilities.lenght,
+                                baseExperience: data.base_experience,
+                                imageUrl: data.sprites.front_default
+                            },
+                            jsonResponse: data.name
+                        })
+                    })
+                }
+            })
+        }else{
+            this.setState({ error: true});
+            return
+        }
+        
+    }
 
     render() {
-        const name1 = this.props.name
-        const numberOfPokemons1 = this.props.numberOfPokemons 
+        const{name: userName, numberOfPokemons} = this.props;
+        const{
+            error, 
+            pokemon
+        } = this.state;
+        
+        let resultMarkUp;
+        let jsonResponse : string = "";
+
         //const{name, numberOfPokemons} = this.props
+
+
+        if (error){
+            resultMarkUp = <p>Pokemon not found, please try again.</p>
+        }else if(this.state.pokemon.name !== ""){
+            resultMarkUp = <div>
+                <img src = {pokemon.imageUrl} alt = "pokemon" className= "pokemon-image"/>
+                <p>
+                    {pokemon.name} has {pokemon.numberOfAbilities} abilities and {pokemon.baseExperience} base experience points
+                </p>
+            </div>
+            jsonResponse = this.state.jsonResponse
+        }
+
         return (
             <div>
                 <p>
-                    User {name1} {' '}
-                    {numberOfPokemons1 && <span> has {numberOfPokemons1} pokemons </span>} 
+                    User {userName} {' '}
+                    {numberOfPokemons && <span> has {numberOfPokemons} pokemons </span>} 
                 </p>
+                <input type ="text" ref = {this.pokemonRef}/>
+                <button onClick = {this.onSearchClick} className = "searchButton">
+                    Search
+                </button>
+                {resultMarkUp}
+                <br/>
+                <br/>
+                <br/>
+                {jsonResponse}
             </div>
         )
     }
